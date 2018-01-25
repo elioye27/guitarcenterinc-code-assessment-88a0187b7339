6f7939461bc1bd2f89be747f6dc25e962b073985
elioye27 <elioye27@gmail.com>
2018-01-25 01:40:02 -0800

    added

--- javascript/app.js
+++ javascript/app.js
@@ -27,6 +27,14 @@ function openTab(evt, cityName) {
     evt.currentTarget.className += " active";
 }
 
+
+//new arrivals
 $.getJSON("../json/new-arrivals.json", function(json) {
     console.log(json); // this will show the info it in firebug console
+    $('#image').attr('src',json.newArrivals[0].image);
+    $('#title').html(json.newArrivals[0].title);
+    $('#description').html(json.newArrivals[0].description);
+    $("#url").attr("href",json.newArrivals[0].url);
+    $("#urlText").html(json.newArrivals[0].url);
+    
 });
