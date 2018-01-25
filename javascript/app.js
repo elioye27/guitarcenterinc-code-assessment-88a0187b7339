94427a9315d9891a6ea797412fec2980cf4e6f4a
elioye27 <elioye27@gmail.com>
2018-01-24 23:23:01 -0800

    add

--- javascript/app.js
+++ javascript/app.js
@@ -1,20 +1,28 @@
-document.onclick = function() {
+//ACCORDION
+var acc = document.getElementsByClassName("accordion");
+for (var i = 0; i < acc.length; i++) {
+   acc[i].addEventListener("click", function() {
+       this.classList.toggle("active");
+       var panel = this.nextElementSibling;
+       if (panel.style.display === "block") {
+           panel.style.display = "none";
+       } else {
+           panel.style.display = "block";
+       }
+   });
+}
 
-	var acc = document.getElementsByClassName("accordion");
-	var i;
-
-	for (i = 0; i < acc.length; i++) {
-	  acc[i].addEventListener("click", function() {
-	    this.classList.toggle("active");
-	    var panel = this.nextElementSibling;
-	    if (panel.style.maxHeight){
-	      panel.style.maxHeight = null;
-	    } else {
-	      panel.style.maxHeight = panel.scrollHeight + "px";
-	    } 
-	  });
-	}
-
-	console.log([i]);
-	console.log("hello");
+// TAB CONTROL
+function openTab(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
