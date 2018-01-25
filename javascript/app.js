3c129d0a1e0bff34ab81c4dd745cb92f36fe6365
elioye27 <elioye27@gmail.com>
2018-01-24 18:12:17 -0800

    add

---
+++ javascript/app.js
@@ -0,0 +1,20 @@
+document.onclick = function() {
+
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+	  acc[i].addEventListener("click", function() {
+	    this.classList.toggle("active");
+	    var panel = this.nextElementSibling;
+	    if (panel.style.maxHeight){
+	      panel.style.maxHeight = null;
+	    } else {
+	      panel.style.maxHeight = panel.scrollHeight + "px";
+	    } 
+	  });
+	}
+
+	console.log([i]);
+	console.log("hello");
+}
